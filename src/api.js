b37a406cba5ffd097162c7a62230a9c82d199a15
Mya <skyusmpp@gmail.com>
2023-08-03 20:06:15 -0700

    added city name and icon

---
+++ src/api.js
@@ -0,0 +1,21 @@
+
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': 'f216a33056msh21b84a52274a7e0p1cfbd3jsn8d65d2d8824c',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
